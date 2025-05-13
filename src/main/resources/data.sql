5d9a2fbd1bda19968843e2b6acafe6e3e48d34b1
Joonas Castrén <joonas_castren@yahoo.com>
2025-05-13 16:53:19 +0300

    Fixed data initialization script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@ VALUES ('Antti', 200.0);
 INSERT INTO customer (name, account_balance)
 VALUES ('Mikko', 300.0);
 
-INSERT INTO game_event (id, customer_id, event_type, created_at, amount, new_balance)
+INSERT INTO game_event (id, customer_id, event_type, created_at, amount, original_balance)
 VALUES (1, 1, 'PURCHASE', now(), 10, 90);
-INSERT INTO game_event (id, customer_id, event_type, created_at, amount)
+INSERT INTO game_event (id, customer_id, event_type, created_at, amount, original_balance)
 VALUES (2, 2, 'PURCHASE', now(), 20, 180);
-INSERT INTO game_event (id, customer_id, event_type, created_at, amount)
+INSERT INTO game_event (id, customer_id, event_type, created_at, amount, original_balance)
 VALUES (3, 3, 'WIN', now(), 30, 270);
